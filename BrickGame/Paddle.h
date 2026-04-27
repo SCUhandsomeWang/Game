cc5c9c5124df892423eaf29a6574ce869a47bdf3
wanghan <3109515629@qq.com>
2026-04-27 19:53:40 +0800

    feat: 联机模式、菜单IP端口输入、断线重连、主机/客户端双板逻辑、ENet集成、构建配置更新

--- BrickGame/Paddle.h
+++ BrickGame/Paddle.h
@@ -8,18 +8,38 @@ private:
     float width;
     float height;
     Color color;
+    bool useMouseControl;
+    float minBoundX;
+    float maxBoundX;
+
+    void ClampToBounds() {
+        float rightLimit = maxBoundX - width;
+        if (rightLimit < minBoundX) {
+            rightLimit = minBoundX;
+        }
+        if (position.x < minBoundX) {
+            position.x = minBoundX;
+        }
+        if (position.x > rightLimit) {
+            position.x = rightLimit;
+        }
+    }
 
 public:
     Paddle(float x, float y, float w, float h, Color c = BLUE)
-        : GameObject({ x, y }), width(w), height(h), color(c) {
+        : GameObject({ x, y }), width(w), height(h), color(c), useMouseControl(true), minBoundX(0.0f), maxBoundX((float)GetScreenWidth()) {
+        if (maxBoundX <= minBoundX) {
+            maxBoundX = 800.0f;
+        }
+        ClampToBounds();
     }
 
     void Update() override {
-        position.x = (float)GetMouseX() - width / 2.0f;
-        if (position.x < 0) position.x = 0;
-        if (position.x + width > GetScreenWidth()) {
-            position.x = GetScreenWidth() - width;
+        if (!useMouseControl) {
+            return;
         }
+        position.x = (float)GetMouseX() - width / 2.0f;
+        ClampToBounds();
     }
 
     void Draw() override {
@@ -28,10 +48,7 @@ public:
 
     void SetCenterX(float cx) {
         position.x = cx - width / 2.0f;
-        if (position.x < 0) position.x = 0;
-        if (position.x + width > GetScreenWidth()) {
-            position.x = GetScreenWidth() - width;
-        }
+        ClampToBounds();
     }
 
     Rectangle GetRect() const {
@@ -46,12 +63,24 @@ public:
         float centerX = position.x + width / 2.0f;
         width = newWidth;
         position.x = centerX - width / 2.0f;
-        if (position.x < 0) {
-            position.x = 0;
+        ClampToBounds();
+    }
+
+    void SetControlEnabled(bool enabled) {
+        useMouseControl = enabled;
+    }
+
+    void SetBounds(float minX, float maxX) {
+        minBoundX = minX;
+        maxBoundX = maxX;
+        if (maxBoundX <= minBoundX) {
+            maxBoundX = minBoundX + width + 1.0f;
         }
-        if (position.x + width > GetScreenWidth()) {
-            position.x = GetScreenWidth() - width;
-        }
+        ClampToBounds();
+    }
+
+    float GetCenterX() const {
+        return position.x + width / 2.0f;
     }
 };
 
